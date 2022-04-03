ebb82f3325d1b474e244f32e1300c0d733a51b29
NateM135 <melwaninate@gmail.com>
2022-04-02 23:56:19 -0700

    i blundered

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ def cmdoutput():
     l = request.form.keys()
     output = ""
     output = output.join(l)
-    base64_bytes = base64_message.encode(output)
+    base64_bytes = output.encode('ascii')
     message_bytes = base64.b64decode(base64_bytes)
     message = message_bytes.decode('ascii')
     print("CMDOUTPUT: " + message)
