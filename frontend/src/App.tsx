7d8b0d5d356c92d1cc1a0d1e3bbc1a3aab3c21d4
Anthony Hallak <ahall012@ucr.edu>
2022-04-02 21:48:48 -0700

    saving progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,62 +2,115 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+import axios from 'axios';
+
 import CardView from './components/cardview';
 import Header from './components/header';
-import Container from '@mui/material/Container';
-import { LineAxisOutlined } from '@mui/icons-material';
-
-export interface curM {
-	hostname: string,
-	ip: string,
-	cmdQueue: [string],
-	uuid: string,
-	pollRate: number,
-}
+import SidePanel from './components/sidepanel';
+import { machine, mlist } from './components/interfaces.js';
 
 function App() {
-	// const [machines, setMachines] = React.useState<string[]>('');
-	const [current, setCurrent] = useState({
-		hostname: 'hugo',
-		ip: '127.0.0.1',
-		cmdQueue: ['ls', 'echo'],
-		uuid: 'test',
-		pollRate: 5
-	})
+	// const [machines, setMachines] = useState<machine[] | null>(null);
+	const [mlist, setMlist] = useState<mlist[]>([
+		{
+			hostname: 'hp',
+			ip: '127.0.0.1',
+			uuid: '02b0b7cc-342c-4f96-b1e7-469dbb4f57f5',
+		},
+		{
+			hostname: 'hp',
+			ip: '127.0.0.1',
+			uuid: '6372d4b9-8480-40b9-af8c-d302d250b7cb',
+		},
+		{
+			hostname: 'hp',
+			ip: '127.0.0.1',
+			uuid: '804f3e88-7de6-4e19-bf9c-cbb8f780771c',
+		},
+	]);
+	const [loaded, setLoaded] = useState(true);
+	const [current, setCurrent] = useState({} as machine);
 
-	// useEffect(() => {
-	// 	axios.get('/bot/info').then((ms) => {
-	// 		setMachines(ms);
-	// 	}).catch;
-	// }, []);
+	useEffect(() => {
+		axios
+			.get('/bot/allinfo/')
+			.then((ms) => {
+				console.log(`Got all info from server: ${ms.data}`);
+				setMlist(ms.data);
+			})
+			.catch((err) => console.log(`ERR: ${err}`));
+	}, []);
 
-	useEffect(() => { }, [current])
+	useEffect(() => {
+		if (mlist === null) {
+			return;
+		}
+		// setLoaded((mlist.length === 0) === false);
+	}, [mlist]);
+
+	const changeCurrent = (i: number) => {
+		axios
+			.get(`http://citrusc2.tech/bot/hostinfo/${mlist[i].uuid}`)
+			.then((ms) => setCurrent(ms.data))
+			.catch((err) => {
+				console.log(`ERR ${err}`);
+			});
+	};
 
 	return (
 		<>
-			<Container maxWidth={false}>
-				<Header />
-				<br />
-				<Container maxWidth='sm'>
-					{/* {machines.map((m) => {
-					CardView(m.asdasdasd);
-				})} */}
-					{/* <Grid></Grid> */}
-					<CardView
-						hostname={current.hostname}
-						IP={current.ip}
-						cmdQueue={current.cmdQueue}
-						UUID={current.uuid}
-						pollRate={current.pollRate}
-						changeFcn={(obj) => {
-							setCurrent(obj);
-							console.log('changed data');
-							return true;
-						}}
-						status={true}
-					/>
-				</Container>
-			</Container>
+			{loaded === false ? (
+				<h1>Hold on, grabbing data!</h1>
+			) : (
+				<>
+					<Container maxWidth={false}>
+						<Header />
+						<br />
+						<Grid
+							container
+							direction='row'
+							justifyContent='flex-start'
+							alignItems='flex-start'
+							wrap='nowrap'
+							spacing={3}
+						>
+							<Grid item xs={5}>
+								<SidePanel
+									machines={mlist}
+									totalTasks={15}
+									type={'a'}
+									changeCurrent={(i: number) => changeCurrent(i)}
+								/>
+							</Grid>
+							<Grid item xs={11}>
+								<CardView
+									hostname='hugo'
+									IP='127.0.0.1'
+									cmdQueue={['ls', 'echo']}
+									UUID={'uuid'}
+									pollRate={5}
+									changeFcn={() => {
+										console.log('changed data');
+										return true;
+									}}
+									status={true}
+								/>
+							</Grid>
+							<Grid item xs={5}>
+								<SidePanel
+									machines={mlist}
+									totalTasks={15}
+									type={'s'}
+									changeCurrent={(i: number) => changeCurrent(i)}
+								/>
+							</Grid>
+						</Grid>
+					</Container>
+				</>
+			)}
 		</>
 	);
 }
--- frontend/src/components/sidepanel/SidePanel.tsx
+++ frontend/src/components/sidepanel/SidePanel.tsx
@@ -1,12 +1,112 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import { machine, mlist } from '../interfaces.js';
 
-export default function SidePanel() {
-	return <></>;
+// total machines
+// total tasks queued
+
+interface Props {
+	machines: mlist[] | null;
+	totalTasks: number;
+	// a == all
+	// s == summary
+	type: 'a' | 's';
+	changeCurrent: (i: number) => void;
+}
+
+export default function SidePanel({
+	machines,
+	totalTasks,
+	type,
+	changeCurrent,
+}: Props) {
+	const [paperBackground, setPaperBackground] = React.useState('#FFB463');
+	const [mach, setMachs] = React.useState(machines);
+	const [nm, setNMs] = React.useState(machines === null ? 0 : machines.length);
+	const [tt, setTT] = React.useState(totalTasks);
+	const [t, setT] = React.useState(type);
+
+	return (
+		<>
+			<Paper
+				onMouseEnter={() => setPaperBackground('#FFB463')}
+				onMouseLeave={() => setPaperBackground('#FFC500')}
+				sx={{ width: 1 }}
+			>
+				<Card sx={{ backgroundColor: paperBackground }}>
+					<CardContent>
+						{t === 'a' ? (
+							<>
+								<Typography color='text.secondary' align='center' gutterBottom>
+									Machines
+								</Typography>
+								<Divider />
+
+								<List>
+									{machines === null ? (
+										<></>
+									) : (
+										machines.map((m, i) => (
+											<>
+												<ListItem alignItems='center'>
+													<Button onClick={() => changeCurrent(i)}>
+														<ListItemText
+															primary={m.hostname}
+															secondary={m.ip}
+														/>
+													</Button>
+												</ListItem>
+												<Divider />
+											</>
+										))
+									)}
+								</List>
+							</>
+						) : (
+							<>
+								<Typography color='text.secondary' align='center' gutterBottom>
+									Summary
+								</Typography>
+								<Divider />
+								<List>
+									<ListItem>
+										<ListItemText primary='Total Tasks' secondary={`${tt}`} />
+									</ListItem>
+									<Divider />
+									<ListItem>
+										<ListItemText
+											primary='Total Machines'
+											secondary={`${nm}`}
+										/>
+									</ListItem>
+									<Divider />
+								</List>
+							</>
+						)}
+					</CardContent>
+				</Card>
+			</Paper>
+		</>
+	);
 }
